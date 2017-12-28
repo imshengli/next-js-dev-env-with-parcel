48208f249b3403bb9d3ce2d8ad0af813f22a3450
shengli.zsl <shengli.zsl@alibaba-inc.com>
2017-12-28 16:03:45 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # next-js-dev-env-with-parcel
 Next JS Develop Environment with Parcel
+
+## Parcel
+
+```
+npm install -g parcel-bundler
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './index.css';
+
+const a = async () => {
+    console.log('arrow function');
+    const a1 = await 1;
+};
+
+a();
+
+console.log('Hello, world');
